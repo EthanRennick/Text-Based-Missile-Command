495275cefb3770c04ae7f4822d65e3f9fe16b6e6
Ethan Rennick <45255649+EthanRennick@users.noreply.github.com>
2019-09-24 13:00:03 +0100

    Add files via upload

---
+++ missiles/Text to Bomb/main.cpp
@@ -0,0 +1,31 @@
+/// <summary>
+/// Here's a little game i made
+/// you might want to read it
+/// code for code
+/// but dont worry
+/// shoot missiles
+/// ---
+/// Author:
+/// Ethan Rennick
+/// C00240102
+/// started 24.9.19
+/// ---
+/// Description:
+/// A text based missile command esque game 
+/// ---
+/// For:
+/// A Phil project in Games Programming 
+/// Year 2
+/// ---
+/// Known bugs?
+/// </summary>
+
+
+int main()
+{
+	
+
+
+
+	return 0;
+}
